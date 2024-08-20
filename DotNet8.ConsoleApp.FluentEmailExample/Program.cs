23f4da3ca62e307c5e42ed9df496ffdd5bbc14b3
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-20 11:19:23 +0630

    Add project files.

---
+++ DotNet8.ConsoleApp.FluentEmailExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using FluentEmail.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+
+var fromEmail = "fromEmail@gmail.com";
+var toEmail = "toEmail@gmail.com";
+
+Console.Write("Subject : ");
+string subject = Console.ReadLine()!;
+
+Console.Write("Body : ");
+string body = Console.ReadLine()!;
+
+serviceCollection.AddFluentEmail(fromEmail)
+    .AddSmtpSender("smtp.gmail.com", 587, fromEmail, "");
+
+var services = serviceCollection.BuildServiceProvider();
+
+var _fluentEmail = services.GetRequiredService<IFluentEmail>();
+
+var response = await _fluentEmail
+    .To(toEmail)
+    .Subject(subject)
+    .Body(body)
+    .SendAsync();
+
+Console.WriteLine("Message was sent => " + response.Successful);
+Console.ReadLine();
