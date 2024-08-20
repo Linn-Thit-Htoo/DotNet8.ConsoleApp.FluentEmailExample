fe7719ac1b98c45b76cd2d1a46f9b2fdfdb51a12
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-20 11:44:33 +0630

    -

--- DotNet8.ConsoleApp.FluentEmailExample/Program.cs
+++ DotNet8.ConsoleApp.FluentEmailExample/Program.cs
@@ -12,18 +12,13 @@ string subject = Console.ReadLine()!;
 Console.Write("Body : ");
 string body = Console.ReadLine()!;
 
-serviceCollection.AddFluentEmail(fromEmail)
-    .AddSmtpSender("smtp.gmail.com", 587, fromEmail, "");
+serviceCollection.AddFluentEmail(fromEmail).AddSmtpSender("smtp.gmail.com", 587, fromEmail, "");
 
 var services = serviceCollection.BuildServiceProvider();
 
 var _fluentEmail = services.GetRequiredService<IFluentEmail>();
 
-var response = await _fluentEmail
-    .To(toEmail)
-    .Subject(subject)
-    .Body(body)
-    .SendAsync();
+var response = await _fluentEmail.To(toEmail).Subject(subject).Body(body).SendAsync();
 
 Console.WriteLine("Message was sent => " + response.Successful);
 Console.ReadLine();
